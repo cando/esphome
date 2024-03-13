fc0a6f83475682e8c1eed18b48476a06a910e1ba
Stefano Candori <stefano.candori@prima.it>
2024-03-13 09:18:24 +0100

    Update __init__.py

--- esphome/components/bme68x_bsec/__init__.py
+++ esphome/components/bme68x_bsec/__init__.py
@@ -70,7 +70,8 @@ async def to_code(config):
     cg.add_define("USE_BSEC2")
     cg.add_library(
         "BME68x Sensor library",
-        "1.1.40407",
+        None,
+        "https://github.com/boschsensortec/Bosch-BME68x-Library.git#v1.1.40407",
     )
     cg.add_library(
         "BSEC2 Software Library",
